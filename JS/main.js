0f81c05c5e8c88669ecb3c9b75d7f574cb4af465
Jeferson <jefeleal363@gmail.com>
2025-04-11 21:56:48 -0300

    fix: HTML and CSS finished and stop programming JavaScript for now

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,36 @@
+// * CODIGO INICIAL
 
-const images = {}
+const images = {};
 
 const imagesToLoad = {
-    player: "../images/player.png",
-    enemyA1: "../images/enemyA1.png",
-    enemyA0: "../images/enemyA0.png",
-    enemyB0: "../images/enemyB0.png",
-    enemyB1: "../images/enemyB1.png",
-    enemyC0: "../images/enemyC0.png",
-    enemyC1: "../images/enemyC1.png",
-}
+  player: "../images/player.png",
+  enemyA1: "../images/enemyA1.png",
+  enemyA0: "../images/enemyA0.png",
+  enemyB0: "../images/enemyB0.png",
+  enemyB1: "../images/enemyB1.png",
+  enemyC0: "../images/enemyC0.png",
+  enemyC1: "../images/enemyC1.png",
+};
+
+(function loadImages() {
+  Object.keys(imagesToLoad).forEach((key) => {
+    console.log(key);
+  });
+
+  const img = new Image();
+  img.src = "../images/player.png";
+  images["player"] = {
+    image: img,
+    isLoaded: false,
+  };
+
+  img.onload = () => {
+    images[key].isLoaded = true;
+  };
+})();
+
+// //abstrair de forma que tudo que está dentro de images to load irá funcionar
 
 
-function loadImages() {
-    const img = new Image() 
-    img.src = "../images/player.png"
-    images["player"] = {
-        image: img,
-        isLoaded: false,
-
-    };
-
-    img.onload = () => {
-        images["player"].isLoaded = true;
-    }
 
 
-
-    
-
-}
